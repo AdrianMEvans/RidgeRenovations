758bd705ede831717222a15dc92070caf206dae5
AdrianMEvans <73572438+AdrianMEvans@users.noreply.github.com>
2022-07-20 00:51:43 +0800

    changed adrian@pw to admin@rr

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'adrian@polywhiz.com';
+  $receiving_email_address = 'admin@ridgerenovations.com.au';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -19,7 +19,7 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->mailer = "adrian@polywhiz.com";
+  $contact->mailer = "admin@ridgerenovations.com.au";
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
